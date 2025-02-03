aba21351cf6460b74b8bd8d587b37bfb483eefc3
VictoriaReyes <reyedvictoria1803@gmail.com>
2025-02-02 23:09:10 -0600

    holaa

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,4 +11,17 @@ import { CarouselComponent } from '../carousel/carousel.component';
 })
 export class HomeComponent {
 
+  ngOnInit(): void {
+    // Toggle the navigation menu on hamburger click
+    const hamburger: HTMLElement | null = document.getElementById('hamburger-icon');
+    const navMenu: HTMLElement | null = document.getElementById('nav-menu');
+
+    if (hamburger && navMenu) {
+      hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+
+    });
+    }
+  }
+
 }
